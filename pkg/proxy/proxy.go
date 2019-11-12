13ca07e0b636c3abaeee70bb54f16a5dee042c04
Simon Alling <alling.simon@gmail.com>
2019-11-12 11:05:53 +0100

    Helm 3 actions working

--- pkg/proxy/proxy.go
+++ pkg/proxy/proxy.go
@@ -23,6 +23,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc/status"
+	"helm.sh/helm/v3/pkg/action"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/helm/pkg/helm"
 	"k8s.io/helm/pkg/proto/hapi/chart"
@@ -35,6 +36,7 @@ var (
 )
 
 func init() {
+	log.Printf("%v\n", new(action.Configuration))
 	appMutex = make(map[string]*sync.Mutex)
 	// List of possible statuses obtained from:
 	// https://github.com/helm/helm/blob/master/cmd/helm/list.go#L214
