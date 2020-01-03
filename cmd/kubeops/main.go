3e45b68f3fc1d7fa255b587c896969bfa2fc4e25
Simon Alling <alling.simon@gmail.com>
2020-01-03 10:42:36 +0100

    Add Helm 3 DeleteRelease

--- cmd/kubeops/main.go
+++ cmd/kubeops/main.go
@@ -80,6 +80,9 @@ func main() {
 	apiv1.Methods("GET").Path("/namespaces/{namespace}/releases/{releaseName}").Handler(negroni.New(
 		negroni.Wrap(withAgentConfig(handler.GetRelease)),
 	))
+	apiv1.Methods("DELETE").Path("/namespaces/{namespace}/releases/{releaseName}").Handler(negroni.New(
+		negroni.Wrap(withAgentConfig(handler.DeleteRelease)),
+	))
 
 	// Chartsvc reverse proxy
 	authGate := auth.AuthGate()
